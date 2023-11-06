5730c8cc73d6632cd640cad360d6ebde3cd62388
Joseph Trubenstein <josephtrubenstein@gmail.com>
2023-11-07 08:12:48 +0900

    sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import { manifest } from './src/utils/manifest';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://localhost:4322/',
+  site: 'https://testbmj.netlify.app',
   image: {
     remotePatterns: [{ protocol: 'https' }],
   },
